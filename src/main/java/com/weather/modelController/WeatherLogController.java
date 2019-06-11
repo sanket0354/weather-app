b23ea190cb95a40330300f7c867bd0abdf7d85b6
Sanket Patel <sanket0354@gmail.com>
2019-06-11 12:38:37 -0400

    Defect fix where there were multiple logs or no logs returned

--- src/main/java/com/weather/modelController/WeatherLogController.java
+++ src/main/java/com/weather/modelController/WeatherLogController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceException;
 
 import com.weather.model.Location;
 import com.weather.model.WeatherLog;
@@ -27,11 +28,16 @@ public class WeatherLogController {
 	/**
 	 * Add a new weather log fetched from the api
 	 * 
-	 * @param temperature   the current temperature of the location
-	 * @param windSpeed     the current windspeed of the location
-	 * @param conditionType the current condition type of the the location
-	 * @param location      the location to add log for
-	 * @param source        the api source from which the data is coming from
+	 * @param temperature
+	 *            the current temperature of the location
+	 * @param windSpeed
+	 *            the current windspeed of the location
+	 * @param conditionType
+	 *            the current condition type of the the location
+	 * @param location
+	 *            the location to add log for
+	 * @param source
+	 *            the api source from which the data is coming from
 	 * @return the WeatherLog object added to the database
 	 */
 	public WeatherLog addLog(double temperature, double windSpeed, String conditionType, Location location,
@@ -49,9 +55,11 @@ public class WeatherLogController {
 	}
 
 	/**
-	 * Add a new weather log based on the WeatherLog object passed as an parameter
+	 * Add a new weather log based on the WeatherLog object passed as an
+	 * parameter
 	 * 
-	 * @param weatherLog the log to add
+	 * @param weatherLog
+	 *            the log to add
 	 * @return the loged added to the database
 	 */
 	public WeatherLog addLog(WeatherLog weatherLog) {
@@ -66,18 +74,29 @@ public class WeatherLogController {
 	}
 
 	/**
-	 * Find a WeatherLog based on the location and the source id from the database
+	 * Find a WeatherLog based on the location and the source id from the
+	 * database
 	 * 
-	 * @param location the location for which the logs need to be retrieved
-	 * @param source   the api source that will correspond the location
+	 * @param location
+	 *            the location for which the logs need to be retrieved
+	 * @param source
+	 *            the api source that will correspond the location
 	 * @return the WeatherLog fetched from the database
 	 */
 	public WeatherLog findLastLogBasedOnLocationAndSourceAndDate(Location location, SourceType source) {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		WeatherLog weatherLog = (WeatherLog) entityManager
-				.createQuery("SELECT a FROM WeatherLog a where a.location = :location_id AND a.sourceId = :source_id")
-				.setParameter("location_id", location).setParameter("source_id", source.getValue()).getSingleResult();
-
+		WeatherLog weatherLog = null;
+		try {
+			// in case no result is found, catch the exception and return null
+			weatherLog = (WeatherLog) entityManager
+					.createQuery(
+							"SELECT a FROM WeatherLog a where a.location = :location_id AND a.sourceId = :source_id ORDER BY a.id DESC")
+					.setParameter("location_id", location).setParameter("source_id", source.getValue()).setMaxResults(1)
+					.getSingleResult();
+		} catch (PersistenceException ex) {
+			ex.printStackTrace();
+			return null;
+		}
 		if (weatherLog == null) {
 			return null;
 		}
